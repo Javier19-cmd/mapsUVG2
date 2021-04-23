211b6305b0e9a62b3b80b947cfcc038d00946f06
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-04-23 15:18:18 -0600

    Clase de 23/04/2021

---
+++ app/src/main/java/com/example/mapsuvg/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.mapsuvg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    //Variables
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createFragment()
+    }
+
+    private fun createFragment()
+    {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap)
+    {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker()
+    {
+        val coordinates = LatLng(14.603896958564876, -90.48922654563204)
+
+        val marker = MarkerOptions().position(coordinates).title("Mi U")
+
+        map.addMarker(marker)
+
+        map.animateCamera(
+
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
+            4000,
+            null
+        )
+    }
+
+}
